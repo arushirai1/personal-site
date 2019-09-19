ea8841eae368ad5dadea89ba3e2af2e5302f738e
Arushi Rai <arushirai1999@gmail.com>
2019-09-19 12:32:52 -0500

    Fix broken url locally

--- publish.sh
+++ publish.sh
@@ -1,29 +1,9 @@
-
-#!/bin/sh
-
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
-echo "Deleting old publication"
 rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+git subtree add --prefix=public https://github.com/arushirai1/personal-site.git gh-pages --squash
 hugo
+git add -A
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git commit -m "Publish"
 
-echo "Pushing to github"
+git push origin master
 git subtree push --prefix=public https://github.com/arushirai1/personal-site.git gh-pages
